24516503f4480f64f69c582f8da4a2216e1e05fb
cristophercervantes <eudardosheldon@gmail.com>
2025-10-23 21:58:05 +0600

    Create crawler.go

---
+++ internal/crawler/crawler.go
@@ -0,0 +1,35 @@
+package crawler
+
+import (
+	"net/url"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+// FindVulnerableURLs crawls a root URL to find URLs with query parameters
+func FindVulnerableURLs(root string) []string {
+	candidates := []string{}
+	visited := make(map[string]bool)
+	c := colly.NewCollector(
+		colly.AllowedDomains(rootHost(root)),
+		colly.MaxDepth(2),
+	)
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		parsed, err := url.Parse(link)
+		if err == nil && len(parsed.Query()) > 0 && !visited[link] {
+			visited[link] = true
+			candidates = append(candidates, link+"*") // Append * for payload placeholder
+		}
+	})
+
+	c.Visit(root)
+	return candidates
+}
+
+func rootHost(root string) string {
+	parsed, _ := url.Parse(root)
+	return parsed.Host
+}
