3d338c0508562aa20cb70396c8133ee808a242bc
cristophercervantes <eudardosheldon@gmail.com>
2025-10-23 22:01:30 +0600

    Create utils.go

---
+++ internal/utils/utils.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+// LoadLines reads lines from a file, ignoring empty ones
+func LoadLines(file string) ([]string, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line != "" {
+			lines = append(lines, line)
+		}
+	}
+	return lines, scanner.Err()
+}
